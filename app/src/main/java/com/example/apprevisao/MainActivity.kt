d2d11a5146703342e61858a775ef049111812c5c
EddManBR <dudutavaresjr@gmail.com>
2022-05-02 09:17:45 -0300

    second commit

--- app/src/main/java/com/example/apprevisao/MainActivity.kt
+++ app/src/main/java/com/example/apprevisao/MainActivity.kt
@@ -2,13 +2,29 @@ package com.example.apprevisao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.testButton).setOnClickListener{
+            comparation(it)
+        }
+    }
+
+    private fun comparation(view: View){
+        val num1 = findViewById<EditText>(R.id.number1)
+        val num2 = findViewById<EditText>(R.id.number2)
+        val comparationResult = findViewById<TextView>(R.id.comparration)
+
+        if (num1==num2){
+            comparationResult.text = "num1.toString() = num2.toString()"
+            view.visibility = View.GONE
+            comparationResult.visibility = View.VISIBLE
         }
     }
 }
